892dfa6c94f898f7d44dcaa80ac2e83f2b673cdb
SaraSchoonover <saramschoon@gmail.com>
2021-03-17 20:21:13 -0500

    create modules and read

--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -1,13 +1,18 @@
+import { showAirports } from '../components/airports';
 import domBuilder from '../components/forms/domBuilder';
 import navBar from '../components/forms/navBar';
+import navEvents from '../events/navEvents';
+import getAirports from '../helpers/data/airportData';
 // import loginButton from '../components/loginButton';
 // import logoutButton from '../components/logoutButton';
 
-const startApp = () => {
+const startApp = (userObject) => {
   domBuilder();
   navBar();
+  navEvents(userObject.uid);
   // loginButton();
   // logoutButton();
+  getAirports(userObject.uid).then((airportArray) => showAirports(airportArray));
 };
 
 export default startApp;
