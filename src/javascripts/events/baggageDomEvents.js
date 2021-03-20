defc1043c9ec700a2f4e42076aa0988b0b2c7172
Dani Crosby <dani.crosby1111@gmail.com>
2021-03-20 11:36:21 -0500

    delete functionality working

--- src/javascripts/events/baggageDomEvents.js
+++ src/javascripts/events/baggageDomEvents.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-alert */
 import { showBaggage } from '../components/baggage';
 import addBaggageForm from '../components/forms/addBaggageForm';
-import { createBaggage } from '../helpers/data/baggageData';
+import { createBaggage, deleteBaggage } from '../helpers/data/baggageData';
 
 const baggageDomEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
+    console.warn('hello');
+    if (e.target.id.includes('delete-baggage')) {
+      if (window.confirm('Are you sure you want to delete this bag?')) {
+        const firebaseKey = e.target.id.split('--')[1];
+        deleteBaggage(firebaseKey).then((baggageArray) => showBaggage(baggageArray));
+      }
+    }
+
     if (e.target.id.includes('add-baggage-btn')) {
       addBaggageForm();
     }
